2b3e74d057f68cdf7d741fec852cc43d78fdc170
guybenda <guy.bend@gmail.com>
2021-06-23 01:21:31 +0300

    Initial version (for history)

--- lolwatcher2/Program.cs
+++ lolwatcher2/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Management;
@@ -9,62 +8,24 @@ namespace lolwatcher2
 {
     class Program
     {
-        static List<string> LOCALES = new List<string>{
-            "cs_CZ",
-            "el_GR",
-            "pl_PL",
-            "ro_RO",
-            "hu_HU",
-            "en_GB",
-            "de_DE",
-            "es_ES",
-            "it_IT",
-            "fr_FR",
-            "ja_JP",
-            "ko_KR",
-            "es_MX",
-            "es_AR",
-            "pt_BR",
-            "en_US",
-            "en_AU",
-            "ru_RU",
-            "tr_TR",
-            "ms_MY",
-            "en_PH",
-            "en_SG",
-            "th_TH",
-            "vn_VN",
-            "id_ID",
-            "zh_MY",
-            "zh_CN",
-            "zh_TW"
-        }.OrderBy(l => l).ToList();
-
         static void Main(string[] args)
         {
             string lang;
-            int counter = 0;
 
             if (args.Length == 0 || string.IsNullOrEmpty(args[0]))
             {
 
                 Console.WriteLine("Enter target language:");
-                LOCALES.ForEach(l => Console.WriteLine(counter++ + " " + l));
+                Console.WriteLine("cs_CZ el_GR pl_PL ro_RO hu_HU en_GB de_DE es_ES it_IT fr_FR ja_JP ko_KR es_MX es_AR pt_BR en_US en_AU ru_RU tr_TR ms_MY en_PH en_SG th_TH vn_VN id_ID zh_MY zh_CN zh_TW");
 
                 lang = Console.ReadLine();
 
-                while (string.IsNullOrWhiteSpace(lang) || LOCALES.Where(l => l == lang).Any())
+                while (string.IsNullOrWhiteSpace(lang))
                 {
                     Console.WriteLine("Enter target language:");
-                    LOCALES.ForEach(l => Console.WriteLine(counter++ + " " + l));
+                    Console.WriteLine("cs_CZ el_GR pl_PL ro_RO hu_HU en_GB de_DE es_ES it_IT fr_FR ja_JP ko_KR es_MX es_AR pt_BR en_US en_AU ru_RU tr_TR ms_MY en_PH en_SG th_TH vn_VN id_ID zh_MY zh_CN zh_TW");
 
                     lang = Console.ReadLine();
-
-                    int i;
-                    if (int.TryParse(lang, out i))
-                    {
-                        lang = LOCALES[i];
-                    }
                 }
             }
             else
@@ -72,15 +33,15 @@ namespace lolwatcher2
                 lang = args[0];
             }
 
-            Console.WriteLine("Language changer active.");
-
             while (true)
             {
+                Console.WriteLine("Waiting for game to start...");
+
                 Process[] t = Process.GetProcessesByName("League of Legends");
                 while (t.Length == 0)
                 {
                     t = Process.GetProcessesByName("League of Legends");
-                    Thread.Sleep(500);
+                    Thread.Sleep(50);
                 }
 
                 string cmd;
@@ -91,16 +52,37 @@ namespace lolwatcher2
                     cmd = objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
                 }
 
-                if (cmd.Contains(lang))
-                {
-                    continue;
-                }
-
-                LOCALES.ForEach(l => cmd.Replace(l, lang));
-
                 ProcessStartInfo lol = new ProcessStartInfo(
                     cmd
-                 );
+                        .Replace("cs_CZ", lang)
+                        .Replace("el_GR", lang)
+                        .Replace("pl_PL", lang)
+                        .Replace("ro_RO", lang)
+                        .Replace("hu_HU", lang)
+                        .Replace("en_GB", lang)
+                        .Replace("de_DE", lang)
+                        .Replace("es_ES", lang)
+                        .Replace("it_IT", lang)
+                        .Replace("fr_FR", lang)
+                        .Replace("ja_JP", lang)
+                        .Replace("ko_KR", lang)
+                        .Replace("es_MX", lang)
+                        .Replace("es_AR", lang)
+                        .Replace("pt_BR", lang)
+                        .Replace("en_US", lang)
+                        .Replace("en_AU", lang)
+                        .Replace("ru_RU", lang)
+                        .Replace("tr_TR", lang)
+                        .Replace("ms_MY", lang)
+                        .Replace("en_PH", lang)
+                        .Replace("en_SG", lang)
+                        .Replace("th_TH", lang)
+                        .Replace("vn_VN", lang)
+                        .Replace("id_ID", lang)
+                        .Replace("zh_MY", lang)
+                        .Replace("zh_CN", lang)
+                        .Replace("zh_TW", lang)
+                );
 
                 lol.UseShellExecute = false;
 
@@ -115,7 +97,11 @@ namespace lolwatcher2
 
                 Process.Start(lol);
 
-                Console.WriteLine("In-game language changed.");
+                while (Console.KeyAvailable)
+                    Console.ReadKey(false);
+
+                Console.WriteLine("In-game language changed, press any key to start listening again.");
+                Console.ReadKey(false);
             }
         }
     }
